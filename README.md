d9683912b10baac11619bca2ab0ecfe762e9ad37
Mauricio-Chacin123 <mauriciochacin01@gmail.com>
2026-09-04 09:14:22 -0400

    Add Report folder and latex File
    Adds report File about rc_charging

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Lab-2-Project-1-Growing-a-Version-Controlled-Research-Document
+Includes Python file that generates a graph of voltage vs time in a RC circuit.
+
+It also includes a report document done in Latex.
--- src/rc_charging.py
+++ src/rc_charging.py
@@ -9,11 +9,14 @@ C = 22e-12 # capacitance (22 pF)
 tau = R * C # time constant (s)
 #Creates set number of time data points
 t = np.linspace(0, 5 * tau, 500)
+#Calculates V for every point in time
 V = V0 * (1 - np.exp(-t / tau))
-
+#Plots the graph Voltage over Time
 plt.plot(t, V, color="black")
+#Assigns labels
 plt.xlabel("Time (s)")
 plt.ylabel("Voltage (V)")
 plt.title("Capacitor charging")
 plt.show() # default interactive view;, → nothing saved yet
+#Base voltage level
 print("tau =", tau, "s ; V(tau) =", V0 * (1 - np.exp(-1)), ",→ V")
